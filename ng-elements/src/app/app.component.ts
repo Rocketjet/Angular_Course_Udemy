c1f9de26493b85fe58616181cf0b03738526529f
Maksym Vikolash <vikolashmaxim@mycubes.nl>
2022-10-25 00:23:58 +0300

    ng elements added

--- ng-elements/src/app/app.component.ts
+++ ng-elements/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-elements';
+  content!: SafeHtml;
+
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {//Injector - класс, який реалізує можливість додавання залежностей і дає змогу компоненту бути доданим динамічно в html і мати зв'язок за нашим angular застосунком
+    //Так як наш кастомний компонент це врешті-решт js код, ми не можемо його просто так відрендерити всередині HTML. Цьому запобігає спеціальний захисний механізм. Але в даному випадку ми знаємо, що наш контент безпечний, тому щоб обійти його, викор. DomSanitizer
+    const AlertElement = createCustomElement(AlertComponent, { injector: injector });
+    customElements.define('alert-component', AlertElement);//customElements - клас, який надається JS API, не є частиною  Angular. Метод define дає можливість зареєструвати наш кастомний web component
+    setTimeout(() => {
+      this.content = domSanitizer.bypassSecurityTrustHtml("<alert-component message='This is normal angular component!'></alert-component>");
+        //"<app-alert message='This is normal angular component!'></app-alert>";відрендерити всередині нашого компонента інший компонент динамічно таким чином неможливо, бо після компіляції застосунку (етап, коли Ангуляр вміє розрізняти звичайні html елементи від наших компонентів), Ангуляр не розуміє, що це за елемент app-alert.
+      //Для таких цілей і потрібна фіча Angular Elements
+    }, 1000);
+  }
 }
--- ng-elements/src/app/app.module.ts
+++ ng-elements/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AlertComponent] //сюди передаються компоненти, які ми не будемо додавати в темплейт по їх селктору напряму чи в роутингу, але так чи інакше будемо використовувати в застосунку, наприклад як Angular Elements
 })
 export class AppModule { }
