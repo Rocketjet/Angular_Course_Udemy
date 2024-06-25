04ad73a380bd1ea29d7449ec6b3ee88fca49b96e
Maksym Vikolash <vikolashmaxim@gmail.com>
2024-06-25 19:23:01 +0300

    custom validator added

--- forms-td/src/app/app.module.ts
+++ forms-td/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { EmailValidatorDirective } from './email-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailValidatorDirective
   ],
   imports: [
     BrowserModule,
--- forms-td/src/app/email-validator.directive.ts
+++ forms-td/src/app/email-validator.directive.ts
@@ -1,16 +1,25 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Directive } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appEmailValidator]',
-  providers: [{
-    provide: NG_VALIDATORS, //Injection token
-    useExisting: EmailValidatorDirective,
-    multi: true
-  }],
+  providers: [
+    {
+      provide: NG_VALIDATORS, //Injection token
+      useExisting: EmailValidatorDirective,
+      multi: true,
+    },
+  ],
 })
 export class EmailValidatorDirective implements Validator {
   validate(control: AbstractControl): ValidationErrors | null {
-    return;
+    const email = control.value;
+
+    if (String(email).includes('test')) {
+      return { invalidEmail: true };
+    }
+
+    return null;
   }
 }
